595fdedcc95cebf7521f32f02e5127211c1b3bcb
Rich Baldry <rich@baldrys.ca>
2019-09-12 22:13:25 -0700

    Updated to use -k for insecure, switched to using bool and disabled CURLOPT_SSL_VERIFYHOST

--- doh.c
+++ doh.c
@@ -620,7 +620,7 @@ struct dnsprobe {
 
 static int initprobe(struct dnsprobe *p, int dnstype, char *host,
                      const char *url, CURLM *multi, int trace_enabled,
-                     struct curl_slist *headers, long insecure_mode)
+                     struct curl_slist *headers, bool insecure_mode)
 {
   CURL *curl;
   p->dohlen = doh_encode(host, dnstype, p->dohbuffer, sizeof(p->dohbuffer));
@@ -666,7 +666,8 @@ static int initprobe(struct dnsprobe *p, int dnstype, char *host,
     curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2TLS);
     curl_easy_setopt(curl, CURLOPT_PRIVATE, p);
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, insecure_mode);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, insecure_mode?0L:1L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, insecure_mode?0L:1L);
 
     p->curl = curl;
 
@@ -717,13 +718,12 @@ int main(int argc, char **argv)
   int exit_status = 0;
   int queued;
   int url_argc = 1;
-  long insecure_mode = 1L;
+  bool insecure_mode = false;
 
   if(argc > 1) {
     char *opts = argv[1];
     if(opts[0] == '-') {
-      do {
-        opts++;
+      while(*++opts) {
         switch(*opts) {
         case 'v':
           trace_enabled = 1;
@@ -731,10 +731,11 @@ int main(int argc, char **argv)
         case 't':
           test_mode = 1;
           break;
-        case 'i':
-          insecure_mode = 0L;
+        case 'k':
+          insecure_mode = true;
           break;
         case 'h':
+        default:
           help();
           break;
         }
@@ -744,6 +745,10 @@ int main(int argc, char **argv)
   }
   else
     help();
+
+  if(url_argc>=argc)
+    help();
+
   host = argv[url_argc];
   if(argc > 1 + url_argc)
     url = argv[url_argc + 1];
